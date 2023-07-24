421fe948c567dbb32e96249abcc200d4f600dc2e
Sion Kang <siontama@gmail.com>
2023-07-25 01:47:26 +0900

    event

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function Header(props) {
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
 
@@ -10,7 +13,12 @@ function Nav(props) {
   const lis = [];
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={"/read/"+t.id}>{t.title}</a></li>);
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={"/read/"+t.id} onClick={(event)=>{
+        event.preventDefault();
+        props.onChnageMode(event.target.id);
+      }}>{t.title}</a>
+    </li>);
   }
   return <nav>
     <ol>
@@ -36,8 +44,12 @@ function App() {
   ];
   return (
     <div>
-      <Header title="REACT"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="REACT" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChnageMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB!"></Article>
     </div>
   );
