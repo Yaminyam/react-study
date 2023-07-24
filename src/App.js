63b74088410c9d240260f7a1fe0bd26dfad72f25
Sion Kang <siontama@gmail.com>
2023-07-25 00:37:58 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 
-function Header() {
+function Header(props) {
   return <header>
-    <h1><a href="/">WEB</a></h1>
+    <h1><a href="/">{props.title}</a></h1>
   </header>
 }
 
-function Nav() {
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={"/read/"+t.id}>{t.title}</a></li>);
+  }
   return <nav>
     <ol>
       <li><a href="/">HTML</a></li>
@@ -16,19 +21,24 @@ function Nav() {
   </nav>
 }
 
-function Article() {
+function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB!
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: 'HTML', body: 'HTML is ...' },
+    { id: 2, title: 'CSS', body: 'CSS is ...' },
+    { id: 3, title: 'JavaScript', body: 'JavaScript is ...' }
+  ];
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="REACT"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB!"></Article>
     </div>
   );
 }
